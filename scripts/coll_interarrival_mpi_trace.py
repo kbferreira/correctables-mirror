48206659b28408ecaf1f198cf567dd51497bfa31
Kurt B. Ferreira <kbferre@sandia.gov>
2018-03-27 14:37:50 -0600

    adding a dirt simple script to track collective interarriva times in the traces

--- scripts/coll_interarrival_mpi_trace.py
+++ scripts/coll_interarrival_mpi_trace.py
@@ -23,20 +23,25 @@ for (appname, (directory,process_count)) in sorted(workloads.items()):
   # Number of zeros in Rank 0 filename is based on process count
   zeros = int(math.floor(math.log(process_count, 10)) + 1) * "0"
   filename = root_trace_dir + directory + ("/%d/rank-%s.txt" % (process_count, zeros))
+  last_time = None
   with open(filename, "r") as f:
     outfile = open(appname + "-interarrival.txt", "w")
+    outfile.write( "# " + filename + "\n")
+
     for i, line in enumerate( f ):
       mpi_op_match = mpi_op_re.match( line )
       if mpi_op_match == None:
 #       assert "MPI_" not in line or "//" in line
         continue
-  
+
       mpi_op = mpi_op_match.group( 1 )
       mpi_op_time = int( mpi_op_match.group( 3 ) )
 
-      coll_inter[ i ] = mpi_op_time 
+      if last_time == None:
+              last_time = mpi_op_time
+              continue
 
-  outfile.write( "# " + filename + "\n")
-  
-  for ( lnum, time ) in coll_inter.items():
-    outfile.write("%d\t %d\n" % ( lnum, time ) )
+      outfile.write( "%d\t %d\n" % (
+              ( mpi_op_time - last_time ),
+              mpi_op_time ) )
+      last_time = mpi_op_time
